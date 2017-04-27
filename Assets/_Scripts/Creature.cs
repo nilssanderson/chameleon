cd4dbdfc5cf9145c5165bd0fb32f23ac5e3f53dd
Nils Sanderson <thebaldwan@gmail.com>
2017-04-27 12:50:18 +0100

    WIP

---
+++ Assets/_Scripts/Creature.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Creature : MonoBehaviour {
+
+
+	// ==========================================================================
+	// Variables
+	// ==========================================================================
+
+	public float health = 20;
+
+	public int attack = 2;
+	public int defense = 1;
+
+	public int level = 1;
+
+
+	// ==========================================================================
+	// Functions
+	// ==========================================================================
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
